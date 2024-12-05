acda3441f8cc6a0b02f5cb59ad7fce883ef003cd
Sven Meisel <meisel@mogic.com>
2024-12-05 17:30:47 +0100

    update solution for day 5 of 2024

--- 2024/5/main.py
+++ 2024/5/main.py
@@ -6,23 +6,6 @@ def swapIndexes(l, aI, bI):
   l[aI], l[bI] = l[bI], l[aI]
   return l
 
-def parse_updates(updates):
-  return [list(map(int, pages.split(","))) for pages in updates]
-
-def parse_rules(rules):
-  parsed_rules = {}
-  for rule in rules:
-    key_page, page = [int(page) for page in rule.split("|")]
-    current_rule_pages = parsed_rules.get(key_page, [])
-    current_rule_pages.append(page)
-    parsed_rules.update({key_page: current_rule_pages})
-
-  return parsed_rules
-
-def middle_page_number_of_update(update):
-  middle_key = floor(len(update) / 2)
-  return update[middle_key]
-
 def validate_update(update, rules):
   return reduce(
     lambda valid, i: valid and all(
@@ -53,29 +36,33 @@ def correct_update(update, rules):
   return update
 
 def part_one(rules, updates):
-  middle_page_numbers = []
-  for update in updates:
-    valid = validate_update(update, rules)
-    if valid:
-      middle_page_numbers.append(middle_page_number_of_update(update))
+  valid_updates = [update for update in updates if validate_update(update, rules) == True]
 
-  return sum(middle_page_numbers)
+  return sum(
+    [update[floor(len(update) / 2)] for update in valid_updates]
+  )
+
 def part_two(rules, updates):
-  middle_page_numbers = []
-  for update in updates:
-    valid = validate_update(update, rules)
-    if valid == False:
-      corrected_update = correct_update(update, rules)
-      middle_page_numbers.append(middle_page_number_of_update(corrected_update))
+  invalid_updates = [update for update in updates if validate_update(update, rules) == False]
+  corrected_updates = [correct_update(update, rules) for update in invalid_updates]
 
-  return sum(middle_page_numbers)
+  return sum(
+    [update[floor(len(update) / 2)] for update in corrected_updates]
+  )
 
 example_file_path = "5/example.txt"
 input_file_path = "5/input.txt"
 
 rules, updates = open_and_split_into_blocks(input_file_path)
-rules = parse_rules(rules)
-updates = parse_updates(updates)
 
-print(f"Part 1: {part_one(rules, updates)}")
-print(f"Part 2: {part_two(rules, updates)}")
+parsed_rules = {}
+for rule in rules:
+  key_page, page = [int(page) for page in rule.split("|")]
+  current_rule_pages = parsed_rules.get(key_page, [])
+  current_rule_pages.append(page)
+  parsed_rules.update({key_page: current_rule_pages})
+
+updates = [list(map(int, pages.split(","))) for pages in updates]
+
+print(f"Part 1: {part_one(parsed_rules, updates)}")
+print(f"Part 2: {part_two(parsed_rules, updates)}")
